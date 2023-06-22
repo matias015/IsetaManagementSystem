e524e55cdcd36233874db1d4ced04595519c8af0
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-06-21 21:33:37 -0300

    Update examenes.blade.php

--- resources/views/Alumnos/Datos/examenes.blade.php
+++ resources/views/Alumnos/Datos/examenes.blade.php
@@ -1,7 +1,31 @@
 @extends('Alumnos.layout')
 
 @section('content')
-<table>
+<main id="fondo-estudiantes">
+    <section class="table">
+<div class="table__header">
+          <h1>Examenes</h1>
+          <div class="tabla_botonera">
+            <div class="contenedor_filtrar">
+              <select class="filtrar">
+                <option>Filtrar</option>
+                <option>Año</option>
+                <option>Materia</option>
+                <option>Nota final</option>
+                <option>Fecha final</option>
+              </select>
+              <i class="ti ti-adjustments i_filtrar"></i>
+            </div>
+              
+            <div class="contenedor_filtrado">
+              <input class="filtrado-busqueda">
+              <i class="ti ti-search i_lupa"></i>
+              <i class="ti ti-x i_borrar"></i>
+            </div>
+          </div>
+        </div>
+<div class="table__body">
+    <table>
     <tr>
         <th>Materia</th>
         <th>Nota mas alta</th>
@@ -16,7 +40,10 @@
     
     @endforeach
     
-</table>
+    </table>
+</div>
+</section>
+</main>
 @endsection
 
            
