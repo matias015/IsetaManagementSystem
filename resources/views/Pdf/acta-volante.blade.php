fba565cc5296367a7c6c6a13e5cc31e91b2e1fc9
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-06-26 15:05:22 -0300

    Update acta-volante.blade.php

--- resources/views/Pdf/acta-volante.blade.php
+++ resources/views/Pdf/acta-volante.blade.php
@@ -12,44 +12,31 @@
         }
 
         .acta_contenedor {
-            background-color: yellow;
-        }
-        
-        .acta_top {
-            display: flex;
-            justify-content: space-between;
-            background-color: red;
-        }
-
-        .acta_info-carrera_bottom {
-            display: flex;
-            justify-content: space-between;
-            background-color: violet;
-        }
-
-        .acta_bottom {
-            display:inline-block;
             width: 100%;
         }
 
-        .acta_info-profesores {
-            width: 60%;
-            background-color: green;
+        .acta_contenedor th {
+            text-align: left;
+            width: 50%;
         }
 
-        table {
+        .acta_info-carrera_bottom td {
+            text-align: left;
+        }
+
+        .tabla1, .tabla2 {
             width: 100%;
         }
 
-        .tabla2 {
-            width: 40%;
+        .tabla1 th {
+            text-align: center;
         }
 
         .tabla2 td {
             min-width: 50px;
         }
 
-        table, th, td {
+        .tabla1, .tabla1 th, .tabla1 td, .tabla2, .tabla2 th, .tabla2 td {
         border: 1px solid black;
         border-collapse: collapse;
         }
@@ -58,99 +45,117 @@
             text-align: center;
         }
 
-        h1 {
-            font-size: 15px;
-        }
         </style>
     </head>
     <body>
-        <div class="acta_contenedor">
-        <div class="acta_top">
-            <span>Provincia de Buenos Aires</span>
-            <h1>ACTA VOLANTE DE EXÁMENES</h1>
-        </div>
-        <div>
-        Dirección General de Cultura y Educación
-        </div>
-        
-
-        <div class="acta_info-carrera">
-            <p>Carrera: {{$mesa->materia->carrera->nombre}}</p>
-            <div class="acta_info-carrera_bottom">
-                <p>Año: {{$mesa->materia->anio+1}}</p>
-                <p>asignatura: {{$mesa->materia->nombre}}</p>
-            </div>
-        </div>
-
-        <table>
+        <table class="acta_contenedor">
             <thead>
-                <tr>
-                    <th rowspan="2">N° Orden</th>
-                    <th rowspan="2">Nombre y Apellido</th>
-                    <th colspan="3">Calificación</th>
-                    <th rowspan="2">N° Documento</th>
+                <tr class="acta_top">
+                    <th colspan="2">Provincia de Buenos Aires</th>
+                    <th colspan="2">ACTA VOLANTE DE EXÁMENES</th>
                 </tr>
                 <tr>
-                    <th>Oral </th>
-                    <th>Escrito</th>
-                    <th>Prom.</th>
+                    <th>Dirección General de Cultura y Educación</th>
                 </tr>
-                </thead>
-                <tbody>
-        @foreach($alumnos as $alumno)
+            </thead>
+            <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>Fecha</td>
+                <td>Hora</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>26/5</td>
+                <td>18.30</td>
+            </tr>
+            <tr>
+                <td colspan="4">Carrera: {{$mesa->materia->carrera->nombre}}</td>
+            </tr>
+            <tr class="acta_info-carrera_bottom">
+                <td>Año: {{$mesa->materia->anio+1}}</td>
+                <td colspan="3">Asignatura: {{$mesa->materia->nombre}}</td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    <table class="tabla1">
+                        <thead>
+                            <tr>
+                                <th rowspan="2">N° Orden</th>
+                                <th rowspan="2">Nombre y Apellido</th>
+                                <th colspan="3">Calificación</th>
+                                <th rowspan="2">N° Documento</th>
+                            </tr>
+                            <tr>
+                                <th>Oral </th>
+                                <th>Escrito</th>
+                                <th>Prom.</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($alumnos as $alumno)
+                            <tr>
+                                <td></td>
+                                <td>{{$alumno->nombre . ' ' . $alumno->apellido}}</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>{{$alumno->dni}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td class="acta_info-profesores" colspan="2">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Presidente: {{$mesa->prof_presidente != 0? $mesa->profesor->nombre . ' ' . $mesa->profesor->apellido:'A confirmar'}}</td>
+                            </tr>
+                            <tr>
+                                <td>1° Vocal: {{$mesa->prof_vocal_1 != 0?
+                        $mesa->vocal1->nombre . ' ' . $mesa->vocal1->apellido:'Sin definir'}}</td>
+                            </tr>
+                            <tr>
+                                <td>2° Vocal: {{$mesa->prof_vocal_2 != 0?
+                $mesa->vocal2->nombre . ' ' . $mesa->vocal2->apellido:'Sin definir'}}</td>
+                            </tr>
+                            <tr>
+                                <td>ISETA, 9 de Julio.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td class="td_tabla2" colspan="2">
+                    <table class="tabla2">
+                        <tbody>
+                            <tr>
+                                <td>Total de alumnos:</td>
+                                <td></td>
+                                </tr>
+                            <tr>
+                                <td>Aprobados:</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Aplazados:</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Ausentes:</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
             
             
-                <tr>
-                    <td></td>
-                    <td>{{$alumno->nombre . ' ' . $alumno->apellido}}</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>{{$alumno->dni}}</td>
-                </tr>
-            
-            @endforeach
-        </tbody>
+            </tbody>
         </table>
-
-        <div class="acta_bottom">
-            <div class="acta_info-profesores">
-                <p>Presidente: {{$mesa->prof_presidente != 0? $mesa->profesor->nombre . ' ' . $mesa->profesor->apellido:'A confirmar'}}</p>
-                <p>1° Vocal: {{$mesa->prof_vocal_1 != 0?
-                        $mesa->vocal1->nombre . ' ' . $mesa->vocal1->apellido:'Sin definir'}}</p>
-
-                <p>2° Vocal: {{$mesa->prof_vocal_2 != 0?
-                $mesa->vocal2->nombre . ' ' . $mesa->vocal2->apellido:'Sin definir'}}</p>
-                <span>ISETA, 9 de Julio.</span>
-            </div>
-
-            <table class="tabla2">
-                <tbody>
-                    <tr>
-                        <td>Total de alumnos:</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Aprobados:</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Aplazados:</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Ausentes:</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        </div>
-        
-    </body>
-    </html>
-
-    </div>
-    
 </body>
 </html>
