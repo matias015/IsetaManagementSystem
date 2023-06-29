1a4df7afc23f18ff2e6b74424fddd15ec00df6f6
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-06-29 18:20:26 -0300

    Update aside.blade.php

--- resources/views/Comp/aside.blade.php
+++ resources/views/Comp/aside.blade.php
@@ -1,17 +1,19 @@
 <aside class="admin-aside">
     
     <ul class="admin-aside-ul">
-        <p>Admin</p>
-        <li><a class="text-blue-600" href="{{route('admin.alumnos.index')}} ">      Alumnos     </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.profesores.index')}}">    Profesores  </a></li>
+        <p>ISETA Admin</p>
+        <li><a class="text-blue-600" href="{{route('admin.alumnos.index')}} "><i class="ti ti-user"></i>      Alumnos     </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.profesores.index')}}"><i class="ti ti-users"></i>    Profesores  </a></li>
         <li><a class="text-blue-600" href="{{route('admin.carreras.index')}}">      Carreras    </a></li>
         <li><a class="text-blue-600" href="{{route('admin.asignaturas.index')}}">   Asignaturas </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.mesas.index')}}">         Mesas       </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.examenes.index')}}">      Examenes    </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.cursadas.index')}}">      Cursadas    </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.admins.index')}}  ">      Admins      </a></li>
-
+        <li><a class="text-blue-600" href="{{route('admin.mesas.index')}}"><i class="ti ti-clipboard-text"></i>         Mesas       </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.examenes.index')}}"><i class="ti ti-address-book"></i>      Examenes    </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.cursadas.index')}}"><i class="ti ti-books"></i></i>      Cursadas    </a></li>
         <hr>
+        <li><a class="text-blue-600" href="{{route('admin.admins.index')}}  "><i class="ti ti-user-cog"></i>      Admins      </a></li>
+        <li class="mode-dark_light"><i class="ti ti-moon"></i>Modo oscuro</li>
+
+        
     </ul>
 
 </aside>
