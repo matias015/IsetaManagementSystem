309743f773a999b5c61bfbb016ea30c438e8001d
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-06-30 15:52:53 -0300

    Update aside.blade.php

--- resources/views/Comp/aside.blade.php
+++ resources/views/Comp/aside.blade.php
@@ -1,7 +1,7 @@
 <aside class="admin-aside">
     
     <ul class="admin-aside-ul">
-        <p>ISETA Admin</p>
+        <p class="logo-iseta">ISETA Admin</p>
         <li><a class="text-blue-600" href="{{route('admin.alumnos.index')}} "><i class="ti ti-user"></i>      Alumnos     </a></li>
         <li><a class="text-blue-600" href="{{route('admin.profesores.index')}}"><i class="ti ti-users"></i>    Profesores  </a></li>
         <li><a class="text-blue-600" href="{{route('admin.carreras.index')}}">      Carreras    </a></li>
@@ -11,9 +11,37 @@
         <li><a class="text-blue-600" href="{{route('admin.cursadas.index')}}"><i class="ti ti-books"></i></i>      Cursadas    </a></li>
         <hr>
         <li><a class="text-blue-600" href="{{route('admin.admins.index')}}  "><i class="ti ti-user-cog"></i>      Admins      </a></li>
-        <li class="mode-dark_light"><i class="ti ti-moon"></i>Modo oscuro</li>
+        <li class="cont-btn">
+            <i class="ti ti-sun-filled"></i>
+            <i class="ti ti-moon"></i>
+            <div id="mode-dark_light">
+                <div class="toggle"></div>
+            </div>
+        </li>
+        
 
         
     </ul>
+
+    <script>
+        var toggle = document.getElementById('mode-dark_light')
+        var body = document.querySelector('body')
+        var aside = document.querySelector('aside')
+        var iseta = document.querySelector('p')
+        var a = document.querySelector('a')
+        var icon_sun = document.querySelector('.ti-sun-filled')
+        var icon_moon = document.querySelector('.ti-moon')
+
+        toggle.onclick = function(){
+            
+            toggle.classList.toggle('active');
+            body.classList.toggle('active');
+            aside.classList.toggle('active');
+            iseta.classList.toggle('active');
+            a.classList.toggle('active');
+            icon_sun.classList.toggle('active');
+            icon_moon.classList.toggle('active');
+        }
+    </script>
 
 </aside>
