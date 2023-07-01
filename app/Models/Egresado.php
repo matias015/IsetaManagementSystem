2a6a9c4eab64546a70f5555393db871ee3c43924
matias015 <78274208+matias015@users.noreply.github.com>
2023-07-01 00:49:44 -0300

    Create Egresado.php
    Egresado

---
+++ app/Models/Egresado.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Egresado extends Model
+{
+    use HasFactory;
+
+    protected $table = "egresadoinscripto";
+    public $timestamps = false;
+
+    protected $fillable = ['id_alumno','id_carrera','anio_inscripcion','indice_libro_matriz','anio_finalizacion'];
+
+    public function alumno(){
+        return $this -> hasOne(Alumno::class,'id','id_alumno');
+    }
+}
