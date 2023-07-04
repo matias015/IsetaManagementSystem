55c25bdafc2a7f9b64a77e1c1be4b5e7ca9fe2d0
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-07-04 20:25:11 -0300

    Update aside.blade.php

--- resources/views/Comp/aside.blade.php
+++ resources/views/Comp/aside.blade.php
@@ -9,12 +9,10 @@
         <li><a class="text-blue-600" href="{{route('admin.mesas.index')}}"><i class="ti ti-clipboard-text"></i>         Mesas       </a></li>
         <li><a class="text-blue-600" href="{{route('admin.examenes.index')}}"><i class="ti ti-address-book"></i>      Examenes    </a></li>
         <li><a class="text-blue-600" href="{{route('admin.cursadas.index')}}"><i class="ti ti-books"></i></i>      Cursadas    </a></li>
-                <li><a class="text-blue-600" href="{{route('admin.egresados.index')}}"><i class="ti ti-books"></i></i>      Egresados    </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.config.index')}}  "><i class="ti ti-user-cog"></i>      Configuracion      </a></li>
-
+        <li><a class="text-blue-600" href="{{route('admin.egresados.index')}}"><i class="ti ti-school"></i>      Egresados    </a></li>
         <hr>
         <li><a class="text-blue-600" href="{{route('admin.admins.index')}}  "><i class="ti ti-user-cog"></i>      Admins      </a></li>
-        <li class="cont-btn">
+        <li><a class="text-blue-600" href="{{route('admin.config.index')}}  "><i class="ti ti-settings"></i>      Configuracion      </a></li>
             <i class="ti ti-sun-filled"></i>
             <i class="ti ti-moon"></i>
             <div id="mode-dark_light">
