923e383d944d4fd7e32b88c682d6e26c3aad6898
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-08-30 14:14:19 -0300

    Update pagination.blade.php

--- resources/views/Componentes/pagination.blade.php
+++ resources/views/Componentes/pagination.blade.php
@@ -4,28 +4,28 @@
 
 <ul class="pager">
     @if ($paginator->onFirstPage())
-        <li class="disabled"><span>← Previous</span></li>
+             <li class="disabled btn1"><span><i class="ti ti-square-rounded-chevron-left"></i>Anterior</span></li>
     @else
-        <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">← Previous</a></li>
+        <li class="btn1"><a href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="ti ti-square-rounded-chevron-left"></i>Anterior</a></li>
     @endif
     @foreach ($elements as $element)
         @if (is_string($element))
-            <li class="disabled"><span>{{ $element }}</span></li>
+            <li class="disabled no-active"><span>{{ $element }}</span></li>
         @endif
         @if (is_array($element))
             @foreach ($element as $page => $url)
                 @if ($page == $paginator->currentPage())
                     <li class="active my-active"><span>{{ $page }}</span></li>
                 @else
-                    <li><a href="{{ $url }}">{{ $page }}</a></li>
+                    <li class="no-active"><a href="{{ $url }}">{{ $page }}</a></li>
                 @endif
             @endforeach
         @endif
     @endforeach
     @if ($paginator->hasMorePages())
-        <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">Next →</a></li>
+        <li class="btn2"><a href="{{ $paginator->nextPageUrl() }}" rel="next">Siguiente<i class="ti ti-square-rounded-chevron-right"></i></a></li>
     @else
-        <li class="disabled"><span>Next →</span></li>
+        <li class="disabled btn2"><span>Siguiente<i class="ti ti-square-rounded-chevron-right"></i></span></li>
     @endif
 </ul>
 
