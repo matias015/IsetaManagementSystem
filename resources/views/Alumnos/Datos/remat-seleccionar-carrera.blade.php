2d27e3a18e797332b0bf06b21684730a8fcd01a9
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-01 15:44:37 -0300

    Update remat-seleccionar-carrera.blade.php

--- resources/views/Alumnos/Datos/remat-seleccionar-carrera.blade.php
+++ resources/views/Alumnos/Datos/remat-seleccionar-carrera.blade.php
@@ -2,18 +2,24 @@
 @section('content')
 
 <main id="fondo-estudiantes">
-    <section class="perfil">
+    <section class="table">
    
         @if (!$en_fecha)
-            <h1>No es fecha de rematriculacion</h1>
+                <div class="table__header">
+                    <h1>No es fecha de rematriculacion</h1>
+                </div>
         @else
-            <div class="black flex-col gap-3 p-3 w-100">
+                <div class="table__header">
+                    <h1>Rematriculación</h1>
+                </div>
+
+                <div class="black flex-col  w-100 gap-3 p-3 ">
                 @foreach ($carreras as $carrera)
                     <form class="grid-1 w-100" action="{{route('alumno.rematriculacion.asignaturas', ['carrera'=>$carrera->id])}}">
-                        <input class="p-3 border-none bg-gray-100 shadow rounded-2 pointer" type="submit" value="{{$carrera->nombre}}">
+                        <input class="p-3 border-none bg-gray-100 shadow rounded-2 pointer style_hover" type="submit" value="{{$carrera->nombre}}">
                     </form>
                 @endforeach
-            </div>
+                </div>
         @endif
 
     </section>
