00170bda61770195bc513c91684d617a51ad8dcd
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-04 18:13:17 -0300

    Update create.blade.php

--- resources/views/Admin/Carreras/create.blade.php
+++ resources/views/Admin/Carreras/create.blade.php
@@ -8,17 +8,18 @@
             @endforeach
         @endif
 
-
+        <div class="perfil_one table">
        <form method="post" action="{{route('admin.carreras.store')}}">
         @csrf
 
-        <p>carrera <input name="nombre"></p>
-        <p>resolucion <input name="resolucion"></p>
-        <p>anio_apertura <input name="anio_apertura"></p>
-        <p>anio_fin <input name="anio_fin"></p>
-        <p>observaciones <input name="observaciones"></p>
+        <span class="perfil_dataname">Carrera: <input class="campo_info" name="nombre"></span>
+        <span class="perfil_dataname">Resolucion: <input class="campo_info" name="resolucion"></span>
+        <span class="perfil_dataname">Año apertura: <input class="campo_info" name="anio_apertura"></span>
+        <span class="perfil_dataname">Año fin: <input class="campo_info" name="anio_fin"></span>
+        <span class="perfil_dataname">Observaciones: <input class="campo_info" name="observaciones"></span>
 
-        <input type="submit" value="Crear">
+        <div class="upd"><input class="btn_borrar upd" type="submit" value="Crear"></div>
        </form>
+        </div>
     </div>
 @endsection
