cd216aab5776c326c8c9da11dc1351f06a5c2f8b
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-04 18:12:46 -0300

    Update create.blade.php

--- resources/views/Admin/Asignaturas/create.blade.php
+++ resources/views/Admin/Asignaturas/create.blade.php
@@ -8,24 +8,26 @@
             @endforeach
         @endif
 
-
+        <div class="perfil_one table">
        <form method="post" action="{{route('admin.asignaturas.store')}}">
         @csrf
 
-        <p>asignatura <input name="nombre"></p>
-        
-        <select name="id_carrera">
+        <span class="perfil_dataname">Asignatura <input class="campo_info" name="nombre"></span>
+        <span class="perfil_dataname">Carrera:
+        <select class="campo_info" name="id_carrera">
             @foreach($carreras as $carrera)
                 <option @selected($id_carrera==$carrera->id) value="{{$carrera->id}}">{{$carrera->nombre}}</option>
             @endforeach
         </select>
-        <p>tipo modulo <input  name="tipo_modulo"></p>
-        <p>carga horaria <input  name="carga_horaria"></p>
-        <p>año<input  name="anio"></p>
-        <p>observaciones <input  name="observaciones"></p>
+        </span>
+        <span class="perfil_dataname">Tipo modulo: <input class="campo_info"  name="tipo_modulo"></span>
+        <span class="perfil_dataname">Carga horaria: <input class="campo_info"  name="carga_horaria"></span>
+        <span class="perfil_dataname">Año:<input class="campo_info"  name="anio"></span>
+        <span class="perfil_dataname">Observaciones: <input class="campo_info"  name="observaciones"></span>
 
 
-        <input type="submit" value="Crear">
+        <div class="upd"><input class="btn_borrar upd" type="submit" value="Crear"></div>
        </form>
+        </div>
     </div>
 @endsection
