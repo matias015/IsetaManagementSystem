8a70e65f45448c4dad91f0b4fe98336dc264af82
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-13 18:49:20 -0300

    Update edit.blade.php

--- resources/views/Admin/Asignaturas/edit.blade.php
+++ resources/views/Admin/Asignaturas/edit.blade.php
@@ -4,97 +4,101 @@
 <div class="edit-form-container">
     <div>
     <div class="perfil_one table">
-
-
-
        <form method="post" action="{{route('admin.asignaturas.update', ['asignatura'=>$asignatura->id])}}">
         @csrf
         @method('put')
 
         <span class="perfil_dataname">Asignatura: <input class="campo_info" value="{{$asignatura->nombre}}" name="nombre"></span>
         <span class="perfil_dataname">Carrera: 
-            <p>{{$asignatura->carrera->nombre}}</p>
+            <p class="campo_info">{{$asignatura->carrera->nombre}}</p>
         </span>
         <span class="perfil_dataname">Tipo modulo: <input class="campo_info" value="{{$asignatura->tipo_modulo}}" name="tipo_modulo"></span>
         <span class="perfil_dataname">Carga horaria: <input class="campo_info" value="{{$asignatura->carga_horaria}}" name="carga_horaria"></span>
         <span class="perfil_dataname">Año:<input class="campo_info" value="{{$asignatura->anio}}" name="anio"></span>
         <span class="perfil_dataname">Observaciones: <input class="campo_info" value="{{$asignatura->observaciones}}" name="observaciones"></span>
-
-        
-
+        <span class="perfil_dataname">Correlativas:
+            <ul class="campo_info">
+                @foreach ($asignatura->correlativas as $correlativa)
+                <form method="post" action="{{route('correlativa.eliminar', ['asignatura'=>$asignatura->id,'asignatura_correlativa'=>$correlativa->asignatura->id])}}">
+                    @csrf
+                    @method('delete')
+                    <li>- {{$correlativa->asignatura->nombre}}</li>
+                    <button class="btn_edit">Eliminar</button>
+                </form>
+                @endforeach
+            </ul>
+    </span>
         <div class="upd"><input class="btn_borrar upd" type="submit" value="Actualizar"></div>
         </form>
     </div>
 
-    <span class="perfil_dataname">Correlativas:
-        <ul class="campo_info">
-            @foreach ($asignatura->correlativas as $correlativa)
-            <form method="post" action="{{route('correlativa.eliminar', ['asignatura'=>$asignatura->id,'asignatura_correlativa'=>$correlativa->asignatura->id])}}">
-                @csrf
-                @method('delete')
-                <li>- {{$correlativa->asignatura->nombre}}</li>
-                <button>Eliminar</button>
-            </form>
-            @endforeach
-        </ul>
-    </span>
-    
+
         {{-- -------------------------- --}}
-        <h2>Agregar correlativa</h2>
 
-       <form method="post" action="{{route('correlativa.agregar', ['asignatura'=>$asignatura->id])}}">
-        @csrf
+        <div class="perfil_one table">
+            <div>
+                <h2>Agregar correlativa</h2>
+            </div>
+            <form method="post" action="{{route('correlativa.agregar', ['asignatura'=>$asignatura->id])}}">
+            @csrf
 
-        <p>
-            materia 
-            <select id="asignatura_select" name="id_asignatura">
-                @foreach ($asignatura->carrera->asignaturas as $asignatura_carrera)
-                    @if ($asignatura_carrera->id != $asignatura->id)
-                        <option value="{{$asignatura_carrera->id}}">{{$asignatura_carrera->nombre}}</option>
-                    @endif
-                @endforeach
+                <span class="perfil_dataname">Materia: 
+                    <select class="campo_info" id="asignatura_select" name="id_asignatura">
+                    @foreach ($asignatura->carrera->asignaturas as $asignatura_carrera)
+                        @if ($asignatura_carrera->id != $asignatura->id)
+                            <option value="{{$asignatura_carrera->id}}">{{$asignatura_carrera->nombre}}</option>
+                        @endif
+                    @endforeach
                
-            </select>
-        </p>
-            <a href=""><button>Agregar</button></a>
-        </form>
+                    </select>
+                </span>
+                <div class="upd"><a href=""><button class="btn_edit">Agregar</button></a></div>
+            </form>
+        </div>
        
-       
-       <hr>
+    <div class="table">
+        <div class="table__header">
+            <p>Alumnos que cursan esta materia que aun no tienen un estado final (aprobado o desaprobado)</p>
+            <input class="btn_borrar" type="submit" value="Imprimir">
+        </div>
        <table>
-        <tr>
-            <td>imprimir</td>
-            <td>nombre</td>
-            <td>dni</td>
-            <td>acciones</td>
-        </tr>
-
-        <form action="{{route('test.print-1', ['asignatura'=>$asignatura->id])}}">
-            alumnos que cursan esta materia que aun no tienen un estado final (aprobado o desaprobado)
-        @foreach ($alumnos as $alumno)
-            <tr>
-                <td><input type="checkbox" checked name="toPrint[]" value="{{$alumno->id}}"></td>
+        <thead>
+                <tr>
+                <th>imprimir</th>
+                <th>nombre</th>
+                <th>dni</th>
+                <th>acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <form action="{{route('test.print-1', ['asignatura'=>$asignatura->id])}}">
                 
-                <td> {{$alumno->nombre}} {{$alumno->apellido}}</td>
+            @foreach ($alumnos as $alumno)
+                <tr>
+                    <td><input type="checkbox" checked name="toPrint[]" value="{{$alumno->id}}"></td>
+                
+                    <td> {{$alumno->nombre}} {{$alumno->apellido}}</td>
 
-                <td> {{$alumno->dni}}</td>
+                    <td> {{$alumno->dni}}</td>
 
-                <td style="display:flex;">
+                    <td style="display:flex;">
                     {{-- <form action="">
-                        <button>Editar</button>
+                        <button class="btn_edit">Editar</button>
                     </form>
                     <form action="">
-                        <button>Eliminar</button>
+                        <button class="btn_edit">Eliminar</button>
                     </form> --}}
-                    <a href="{{route('admin.cursadas.edit', ['cursada' => $alumno->cursada_id])}}">Editar cursada</a>
-                </td>
-            </tr>
-        @endforeach
+                    <a href="{{route('admin.cursadas.edit', ['cursada' => $alumno->cursada_id])}}" class="btn_edit wit">Editar cursada</a>
+                    </td>
+                </tr>
+            @endforeach
 
             
-       <input type="submit" value="Imprimir">
-    </form>
+                
+            </form>
+        </tbody>
     </table>
+    </div>
     </div>
 </div>
 
