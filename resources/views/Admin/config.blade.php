0f4b861365871176d1a6ba3ecb52c689629c184c
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-13 18:55:15 -0300

    Update config.blade.php

--- resources/views/Admin/config.blade.php
+++ resources/views/Admin/config.blade.php
@@ -2,22 +2,20 @@
 
 @section('content')
 
-    <div>
+    <div class="perfil_one table">
 
         <form method="POST" action="{{route('admin.config.set')}}">
             @csrf
-            <p>Filas por tabla <input name="filas_por_tabla" value="{{$configuracion['filas_por_tabla'] ? $configuracion['filas_por_tabla'] : ''}}" name="filas_por_tabla"></p>
-            <p>Horas habiles de inscripcion <input name="horas_habiles_inscripcion" value="{{$configuracion['horas_habiles_inscripcion'] ? $configuracion['horas_habiles_inscripcion'] : ''}}" name="filas_por_tabla"></p>
-            <p>Horas habiles de desinscripcion <input name="horas_habiles_desinscripcion" value="{{$configuracion['horas_habiles_desinscripcion'] ? $configuracion['horas_habiles_desinscripcion'] : ''}}" name="filas_por_tabla"></p>
-            <p>Fechas de rematriculacion 
-                <input type="date" name="fecha_inicial_rematriculacion" value="{{$configuracion['fecha_inicial_rematriculacion'] ? $configuracion['fecha_inicial_rematriculacion'] : ''}}" >
-            </p>
-            <p>Fechas de rematriculacion <input type="date" name="fecha_final_rematriculacion" value="{{$configuracion['fecha_final_rematriculacion'] ? $configuracion['fecha_final_rematriculacion'] : ''}}" name="fecha_final_rematriculacion"></p>
-            <p>Fechas de rematriculacion <input type="number" name="anio_remat" value="{{$configuracion['anio_remat'] ? $configuracion['anio_remat'] : ''}}" name="anio_remat"></p>
-            <p>Fechas limite para revertir rematriculacion <input type="date" name="fecha_limite_desrematriculacion" value="{{$configuracion['fecha_limite_desrematriculacion'] ? $configuracion['fecha_limite_desrematriculacion'] : ''}}"></p>
-            <p>Diferencia maxima entre llamado 1 y 2 en dias <input type="number" name="diferencia_llamados" value="{{$configuracion['diferencia_llamados'] ? $configuracion['diferencia_llamados'] : ''}}"></p>
+            <span class="perfil_dataname">Filas por tabla: <input class="campo_info" name="filas_por_tabla" value="{{$configuracion['filas_por_tabla'] ? $configuracion['filas_por_tabla'] : ''}}" name="filas_por_tabla"></span>
+            <span class="perfil_dataname">Horas habiles de inscripcion: <input class="campo_info" name="horas_habiles_inscripcion" value="{{$configuracion['horas_habiles_inscripcion'] ? $configuracion['horas_habiles_inscripcion'] : ''}}" name="filas_por_tabla"></span>
+            <span class="perfil_dataname">Horas habiles de desinscripcion: <input class="campo_info" name="horas_habiles_desinscripcion" value="{{$configuracion['horas_habiles_desinscripcion'] ? $configuracion['horas_habiles_desinscripcion'] : ''}}" name="filas_por_tabla"></span>
+            <span class="perfil_dataname">Fechas de rematriculacion: <input class="campo_info" type="date" name="fecha_inicial_rematriculacion" value="{{$configuracion['fecha_inicial_rematriculacion'] ? $configuracion['fecha_inicial_rematriculacion'] : ''}}" ></span>
+            <span class="perfil_dataname">Fechas de rematriculacion: <input class="campo_info" type="date" name="fecha_final_rematriculacion" value="{{$configuracion['fecha_final_rematriculacion'] ? $configuracion['fecha_final_rematriculacion'] : ''}}" name="fecha_final_rematriculacion"></span>
+            <span class="perfil_dataname">Fechas de rematriculacion: <input class="campo_info" type="number" name="anio_remat" value="{{$configuracion['anio_remat'] ? $configuracion['anio_remat'] : ''}}" name="anio_remat"></span>
+            <span class="perfil_dataname">Fechas limite para revertir rematriculacion: <input class="campo_info" type="date" name="fecha_limite_desrematriculacion" value="{{$configuracion['fecha_limite_desrematriculacion'] ? $configuracion['fecha_limite_desrematriculacion'] : ''}}"></span>
+            <span class="perfil_dataname">Diferencia maxima entre llamado 1 y 2 en dias: <input class="campo_info" type="number" name="diferencia_llamados" value="{{$configuracion['diferencia_llamados'] ? $configuracion['diferencia_llamados'] : ''}}"></span>
 
-            <button class="m-2 border-none rounded px-3 py-1 bg-blue-900 white">Aplicar</button>
+            <div class="upd"><button class="btn_edit">Aplicar</button></div>
 
         </form>
 
