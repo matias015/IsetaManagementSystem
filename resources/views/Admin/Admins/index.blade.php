91d4e414e6a39195d14ab80e16db937ab74a7bd9
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-13 18:47:40 -0300

    Update index.blade.php

--- resources/views/Admin/Admins/index.blade.php
+++ resources/views/Admin/Admins/index.blade.php
@@ -3,21 +3,15 @@
 @section('content')
 
     <div>
+        <a href="{{route('admin.admins.create')}}"><button>Agregar admin</button></a>
         
-
-    
-            <a href="{{route('admin.admins.create')}}"><button>Agregar admin</button></a>
-
-        <div>
             <form action="{{route('admin.admins.index')}}">
                 <p>filtrar</p> 
                  <input value="{{$filtro}}" name="filtro" type="text">
                  <input type="submit" value="Buscar">
             </form>
-
             <a href="{{route('admin.admins.index')}}"><button>Quitar filtro</button></a>
-      
-        </div>
+    </div>
 
         
 
@@ -27,45 +21,48 @@
         <li class="page-item{{ $page == $alumnos->currentPage() ? ' active' : '' }}">
             <a class="page-link" href="{{ $url }}">{{ $page }}</a>
         </li> --}}
+
+        <div class="perfil_one" style="margin:1em">
+            <form method="POST" action="{{route('admin.admins.store')}}">
+                @csrf
+                <span class="perfil_dataname">Usuario:<input class="campo_info" name="username"></span>
+                <span class="perfil_dataname">Contraseña:<input class="campo_info" name="password"></span>
+                <div class="upd"><input type="submit" value="Crear" class="btn_borrar"></div>
+            </form>
+        </div>
         
-
-        <table>
-            <tr>
-                <td>Id</td>
-                <td>Usuario</td>
-                <td>Acciones</td>
-            </tr>
-
-            <div style="margin:1em">
-                <form method="POST" action="{{route('admin.admins.store')}}">
-                    @csrf
-                    <p>Usuario</p><input name="username">
-                    <p>Contraseña</p><input name="password">
-                    <p><input type="submit" value="Crear"></p>
-                </form>
-            </div>
-
-            @foreach ($admins as $admin)
-            <tr>
-                <td>{{$admin->id}}</td>
-                <td>{{$admin->username}}</td>
-                <td><a href="{{route('admin.admins.edit', ['admin' => $admin->id])}}"><button>editar</button></a></td>
-                <td>
-                    <form method="POST" action="{{route('admin.admins.destroy', ['admin' => $admin->id])}}">
-                        @csrf
-                        @method('delete')
-                        <input type="submit" value="Eliminar">
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-
-        </table>
+        <div class="table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Usuario</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($admins as $admin)
+                    <tr>
+                        <td>{{$admin->id}}</td>
+                        <td>{{$admin->username}}</td>
+                        <td><a href="{{route('admin.admins.edit', ['admin' => $admin->id])}}"><button class="btn_edit">Editar</button></a></td>
+                        <td>
+                            <form method="POST" action="{{route('admin.admins.destroy', ['admin' => $admin->id])}}">
+                                @csrf
+                                @method('delete')
+                                <div class="upd"><input type="submit" value="Eliminar" class="btn_borrar"></div>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         
         <div class="w-1/2 mx-auto p-5">
             {{ $admins->appends(request()->query())->links() }}
         </div>
 
-    </div>
+        
     
 @endsection
