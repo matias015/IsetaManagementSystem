c3919aa674db0e99b827776a8c00e583f8c096d1
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-13 18:51:32 -0300

    Update index.blade.php

--- resources/views/Admin/DiasHabiles/index.blade.php
+++ resources/views/Admin/DiasHabiles/index.blade.php
@@ -1,21 +1,23 @@
 @extends('Admin.template')
 
 @section('content')
-    <h1>dias habiles</h1>
-    <form method="POST" action="{{route('admin.habiles.store')}}">
-        @csrf
-        <input name="fecha" type="date">
-        <button>Agregar</button>
-    </form>
-    <ul>
-        @foreach ($habiles as $habil)
-            <li>{{$habil->fecha}}</li>
-
-            <form method="post" action="{{route('admin.habiles.destroy', ['habil' => $habil->id])}}">
-                @method('delete')
+    <div class="perfil_one table">
+        <h1>Días habiles</h1>
+            <form method="POST" action="{{route('admin.habiles.store')}}">
                 @csrf
-                <button>Eliminar</button>
+                    <input name="fecha" type="date">
+                    <div class="upd"><button class="btn_edit">Agregar</button></div>
             </form>
-        @endforeach
-    </ul>
+            <ul>
+                @foreach ($habiles as $habil)
+                <li>{{$habil->fecha}}</li>
+
+                    <form method="post" action="{{route('admin.habiles.destroy', ['habil' => $habil->id])}}">
+                        @method('delete')
+                        @csrf
+                        <div class="upd"><button class="btn_edit">Eliminar</button></div>
+                    </form>
+                @endforeach
+            </ul>
+    </div>
 @endsection
