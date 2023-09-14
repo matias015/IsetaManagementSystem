422aa38cbd2793b1b6ce843d4c7bb848c201bf6d
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-09-14 05:35:36 -0300

    Update footer.blade.php

--- resources/views/Componentes/footer.blade.php
+++ resources/views/Componentes/footer.blade.php
@@ -1,56 +1,63 @@
 <footer class="font-1 md-font-3">
-    <div class="p-2 grid-1 content gap-3 just-center">
-      <section class="grid-1 gap-2 just-center lg-flex lg-just-between">
-
-        <div class="logo-details center">
-          <span class="font-6 logo_name">ISETA</span>
+    <div class="footer-contenido">
+      <section class="footer-logos grid-1 gap-2 just-center lg-flex lg-just-between">
+        <div class="logo-footer center">
+          <span class="font-6">ISETA</span>
         </div>
-        
-        <div class="flex media-icons just-center">
-          <a class="fb-iseta" href="https://www.facebook.com/iseta9dejulio/" target="_blank">
-            <i class="ti ti-brand-facebook-filled"></i></a>
-          <a class="ig-iseta" href="https://www.instagram.com/isft.iseta/" target="_blank">
-            <i class="ti ti-brand-instagram"></i></a>
-          <a class="sitio-iseta" href="https://iseta.edu.ar/sitio/" target="_blank">
-            <i class="ti ti-world"></i></a>
+        <div class="footer-redes flex just-center">
+          <span>Seguinos en nuestras redes</span>
+          <ul>
+            <li>
+              <a class="col-fb" href="https://www.facebook.com/iseta9dejulio/" target="_blank"><i class="ti ti-brand-facebook-filled"></i></a>
+            </li>
+            <li>
+              <a class="col-ig" href="https://www.instagram.com/isft.iseta/" target="_blank"><i class="ti ti-brand-instagram"></i></a>
+            </li>
+            <li>
+              <a class="col-yt" href="https://iseta.edu.ar/sitio/" target="_blank"><i class="ti ti-world"></i></a>
+            </li>
+          </ul>
         </div>
       </section>
-      <section class="grid-1 md-grid-2 lg-grid-4  gap-2">
-        <ul class="box flex-col items-center md-items-start">
-          <li class="link_name bolder font-4">Explorar</li>
-          <li><a href="{{route('alumno.info')}}">Perfil</a></li>
-          <li><a href="{{route('alumno.cursadas')}}">Cursadas</a></li>
-          <li><a href="{{route('alumno.examenes')}}">Examenes</a></li>
-          <li><a href="{{route('alumno.inscripciones')}}">Inscribir</a></li>
-        </ul>
-        <ul class="box flex-col items-center md-items-start">
-          <li class="link_name bolder font-4">Telefono</li>
-      
-          <li>+54 2317 422305</li>
-          <li>+54 2317 611071</li>
-        </ul>
-        <ul class="box flex-col items-center md-items-start">
-          <li class="link_name bolder font-4">Correos</li>
-     
-          <li>preceptoria@iseta.edu.ar</li>
-          <li>secretaria@iseta.edu.ar</li>
-          <li>iseta@abc.gob.ar</li>
-        </ul>
-        <ul class="box flex-col items-center md-items-start">
-          <li class="link_name bolder font-4">Más información</li>
-
-          <li>H. Yrigoyen 931</li>
-          <li>Lunes a Viernes de 8 a 22.40 hs</li>
-        </ul>
+      <section class="footer-texto grid-1 md-grid-2 lg-grid-4  gap-2">
+        <article class="footer-info">
+          <h3>Explorar</h3>
+          <ul class="box">
+            <li><a href="{{route('alumno.info')}}">Perfil</a></li>
+            <li><a href="{{route('alumno.cursadas')}}">Cursadas</a></li>
+            <li><a href="{{route('alumno.examenes')}}">Examenes</a></li>
+            <li><a href="{{route('alumno.inscripciones')}}">Inscribir</a></li>
+          </ul>
+        </article>
+        <article class="footer-info">
+          <h3>Correos</h3>
+          <ul class="box">
+            <li>preceptoria@iseta.edu.ar</li>
+            <li>secretaria@iseta.edu.ar</li>
+            <li>iseta@abc.gob.ar</li>
+          </ul>
+        </article>
+        <article class="footer-info">
+          <h3>Telefonos</h3>
+          <ul class="box">
+            <li>+54 2317 422305</li>
+            <li>+54 2317 611071</li>
+          </ul>
+        </article>
+        <article class="footer-info">
+          <h3>Más información</h3>
+          <ul class="box">
+            <li>H. Yrigoyen 931</li>
+            <li>Lunes a Viernes de 8 a 22.40 hs</li>
+          </ul>
+        </article>
       </section>
     </div>
-    <div class="p-2 bottom-details">
-      <div class="gray-600 gap-2 grid-1 just-center lg-grid-2">
-        <span class="center copyright_text">Copyright © 2023 <a class="gray-600" href="#">ISETA</a> | Todos los derechos reservados</span>
-        <span class="flex gap-4 just-center policy_terms">
+    <div class="p-2 footer-derechos">
+        <span class="center">Copyright © 2023 <a class="neg" href="#">ISETA</a> | Todos los derechos reservados</span>
+        <span class="flex gap-4 just-center">
           <a class="center" href="#">Política de privacidad</a>
           <a class="center" href="#">Términos & condiciones</a>
         </span>
-      </div>
     </div>
   </footer>
