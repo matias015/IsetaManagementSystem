e23d2b8c03cbb854ad97caf9e7c5cbdbea2ad826
matias015 <78274208+matias015@users.noreply.github.com>
2023-09-18 19:45:36 -0300

    Update AdminsCrudController.php

--- app/Http/Controllers/Admin/AdminsCrudController.php
+++ app/Http/Controllers/Admin/AdminsCrudController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class AdminsCrudController extends Controller
 {
+        function __construct()
+    {
+        $this -> middleware('auth:admin');
+    }
     /**
      * Display a listing of the resource.
      */
