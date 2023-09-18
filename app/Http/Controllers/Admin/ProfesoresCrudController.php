86390ff81418d5d72399c8e7cc1548c44a03334a
matias015 <78274208+matias015@users.noreply.github.com>
2023-09-18 19:46:42 -0300

    Update ProfesoresCrudController.php

--- app/Http/Controllers/Admin/ProfesoresCrudController.php
+++ app/Http/Controllers/Admin/ProfesoresCrudController.php
@@ -12,6 +12,10 @@ use Illuminate\Http\Request;
 
 class ProfesoresCrudController extends Controller
 {
+        function __construct()
+    {
+        $this -> middleware('auth:admin');
+    }
        /**
      * Display a listing of the resource.
      */
