94a31c96d587feb86cf7d28ab603121cf87e2cea
matias015 <78274208+matias015@users.noreply.github.com>
2023-09-18 20:40:21 -0300

    Update alumnos.php

--- routes/alumnos.php
+++ routes/alumnos.php
@@ -21,7 +21,7 @@ Route::prefix('alumno')->group(function(){
     Route::post('/registro',[AlumnoAuthController::class,'registro'])->name('alumno.registro.post');
 
     Route::get('/login',[AlumnoAuthController::class,'loginView'])->name('alumno.login');
-    Route::post('/login',[AlumnoAuthController::class,'login'])->name('alumno.login.post');
+    Route::post('/login',[AlumnoAuthController::class,'login'])->name('alumno.login.post')->middleware('throttle:alumno-login');
 
     Route::get('/logout',[AlumnoAuthController::class,'logout'])->name('alumno.logout');
 
