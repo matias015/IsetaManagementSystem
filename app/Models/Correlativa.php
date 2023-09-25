93b1bda775aef787f17e34abaa97a8968af692e7
matias015 <78274208+matias015@users.noreply.github.com>
2023-09-25 19:34:35 -0300

    Update Correlativa.php

--- app/Models/Correlativa.php
+++ app/Models/Correlativa.php
@@ -10,6 +10,8 @@ class Correlativa extends Model
 {
     use HasFactory;
     protected $table = "correlatividades";
+    protected $fillable = ['id_asignatura','asignatura_correlativa'];
+    public $timestamps = false;
 
     public function asignatura(){
         return $this->BelongsTo(Asignatura::class,'asignatura_correlativa','id');
