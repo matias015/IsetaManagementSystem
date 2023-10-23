024fbf33ef09f3c6434628817c72583182500917
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-10-23 11:14:02 -0300

    Update edit.blade.php

--- resources/views/Admin/Mesas/edit.blade.php
+++ resources/views/Admin/Mesas/edit.blade.php
@@ -1,38 +1,42 @@
 @extends('Admin.template')
 
 @section('content')
-    <div>
-        <div class="edit-form-container">
-            <div class="perfil_one table">
+    <div class="edit-form-container">
+        <div class="perfil_one br">
+            <div class="perfil__header">
                 <h2>{{$mesa->materia->nombre}}</h2>
+            </div>
+            <div class="perfil__info">
                 <form method="post" action="{{route('admin.mesas.update', ['mesa'=>$mesa->id])}}">
                 @csrf
                 @method('put')
 
-                    <span class="perfil_dataname">Fecha: 
-                        <input type="datetime-local" class="campo_info" value="{{$mesa->fecha}}" name="fecha">
-                    </span>
-                    
-                    <span class="perfil_dataname">Llamado: 
-                        <select class="campo_info" name="llamado">
+                    <div class="perfil_dataname">
+                        <label>Fecha:</label>
+                        <input type="datetime-local" class="campo_info rounded" value="{{$mesa->fecha}}" name="fecha">
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Llamado:</label>
+                        <select class="campo_info rounded" name="llamado">
                             <option @selected($mesa->llamado==1) value="1">Primero</option>
                             <option @selected($mesa->llamado==2) value="2">Segundo</option>
                         </select>
-                    </span>
-
-                    <span class="perfil_dataname">Prof. presidente:
-                        <select class="campo_info" name="prof_presidente">
-                            <option @selected($mesa->prof_presidente==0) value="vacio">vacio/A confirmar</option>
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Prof. presidente:</label>
+                        <select class="campo_info rounded" name="prof_presidente">
+                            <option @selected($mesa->prof_presidente==0) value="vacio">Vacio/A confirmar</option>
                             @foreach($profesores as $profesor)
                             <option @selected($mesa->prof_presidente != 0 && $mesa->profesor->id == $profesor->id) value="{{$profesor->id}}">
                                 {{$textFormatService->ucwords($profesor->apellido . ' ' . $profesor->nombre)}}
                             </option>
                             @endforeach
                         </select>
-                    </span>
-                    <span class="perfil_dataname">Prof. vocal 1:
-                        <select class="campo_info" name="prof_vocal_1">
-                            <option @selected($mesa->prof_vocal_1==0) value="vacio">vacio/A confirmar</option>
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Prof. vocal 1:</label>
+                        <select class="campo_info rounded" name="prof_vocal_1">
+                            <option @selected($mesa->prof_vocal_1==0) value="vacio">Vacio/A confirmar</option>
 
                         @foreach($profesores as $profesor)
                             <option @selected($mesa->prof_vocal_1 != 0 && $mesa->vocal1->id == $profesor->id) value="{{$profesor->id}}">
@@ -40,10 +44,11 @@
                             </option>
                         @endforeach
                         </select>
-                    </span>
-                    <span class="perfil_dataname">Prof. vocal 2:
-                        <select class="campo_info" name="prof_vocal_2">
-                            <option @selected($mesa->prof_vocal_2==0) value="vacio">vacio/A confirmar</option>
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Prof. vocal 2:</label>
+                        <select class="campo_info rounded" name="prof_vocal_2">
+                            <option @selected($mesa->prof_vocal_2==0) value="vacio">Vacio/A confirmar</option>
                         @foreach($profesores as $profesor)
                             <option @selected($mesa->prof_vocal_2 != 0 && $mesa->vocal2 && $mesa->vocal2->id == $profesor->id) value="{{$profesor->id}}">
                                 {{$textFormatService->ucwords($profesor->apellido . ' ' . $profesor->nombre)}}
@@ -51,48 +56,52 @@
                             </option>
                         @endforeach
                         </select>
-                    </span>
+                    </div>
 
                     <div class="upd"><input type="submit" value="Actualizar" class="btn_borrar"></div>
                 </form>
             </div>
+        </div>
 
-            <div class="perfil_one table">
+            <div class="perfil_one br">
                 {{-- <p>La funcion de agregar alumnos se elimino hasta que se arreglen algunos errores</p> --}}
-                <h2>Alumnos inscriptos</h2>
+                <div class="perfil__header">
+                    <h2>Alumnos inscriptos</h2>
+                </div>
+                <div class="matricular">
                     <p>Agregar alumno</p>
                     <p>
-
                      @if(strtotime($mesa->fecha) > time())
-        
-                        estos alumnos han aprobado la cursada de esta materia, luego se volvera a validar sobre correlativas y tiempos
-            
+                        Estos alumnos han aprobado la cursada de esta materia, luego se volvera a validar sobre correlativas y tiempos
+                    
                         <form method="POST" action="{{route('admin.examenes.store')}}">
                     @csrf
-                
-                            <select name="id_alumno">
-                                <option value=""></option>
-                            @foreach ($inscribibles as $inscribible)
-                                <option value="{{$inscribible->id}}">{{$textFormatService->ucwords($inscribible->apellido.' '.$inscribible->nombre)}}</option>
-                            @endforeach
-                
+                            <select class="rounded" name="id_alumno">
+                                    <option value=""></option>
+                                @foreach ($inscribibles as $inscribible)
+                                    <option value="{{$inscribible->id}}">{{$textFormatService->ucwords($inscribible->apellido.' '.$inscribible->nombre)}}</option>
+                                @endforeach
                             </select>
                             <input name="id_mesa" value="{{$mesa->id}}" type="hidden">
-                            <input type="submit" value="Agregar">
+                            <div class="upd"><input type="submit" value="Agregar" class="btn_borrar"></div>
                         </form>
+                    
                     @else
                         Ya no se pueden agregar alumnos    
                     @endif
                     </p>
+                </div>
             </div>
             <div class="table">
                 <div class="table__header">
-                    <a href="{{route('admin.mesas.acta', ['mesa'=>$mesa->id])}}" target="_blank"><button class="btn_edit wit3">Acta volante</button></a>
+                    <h2>Acta volante</h2>
+                    <div class="flex just-center">
+                        <a href="{{route('admin.mesas.acta', ['mesa'=>$mesa->id])}}" target="_blank"><button class="btn_edit wit3">Regular</button></a>
+                        <a href="{{route('admin.mesas.actaprom', ['mesa'=>$mesa->id])}}" target="_blank"><button class="btn_edit wit3">Promocion</button></a>
+                    </div>
                 </div>
-                <div class="table__header">
-                    <a href="{{route('admin.mesas.actaprom', ['mesa'=>$mesa->id])}}" target="_blank"><button class="btn_edit wit3">Acta volante promocion</button></a>
-                </div>
-                <table>
+        
+                <table class="table__body">
                     <thead>
                         <tr>
                             <th>Alumno</th>
@@ -104,7 +113,6 @@
                         @foreach($alumnos as $alumno)
                         <tr>
                             <td>{{$textFormatService->ucwords($alumno->apellido . ' ' . $alumno->nombre)}}</td>
-                            
                             <td>{{$alumno->nota!=0? $alumno->nota : 'Sin nota'}}</td>
                             <td>
                                 <a href="{{route('admin.examenes.edit', ['examen' => $alumno->id_examen])}}">
@@ -113,7 +121,7 @@
                                 {{-- <form method="POST" action="{{route('admin.examenes.destroy', ['examen' => $alumno->id_examen])}}">
                                     @csrf
                                     @method('DELETE')
-                                    <input type="submit" value="Eliminar">
+                                    <input type="submit" value="Eliminar" class="btn_borrar">
                                     </form> 
                                 --}}
                             </td>
@@ -122,6 +130,5 @@
                     </tbody>
                 </table>
             </div>
-        </div>
     </div>
 @endsection
