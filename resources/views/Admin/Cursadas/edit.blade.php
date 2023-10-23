ea7a8d21fc0039346952a03e54c3389b4f00cab2
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-10-23 11:05:30 -0300

    Update edit.blade.php

--- resources/views/Admin/Cursadas/edit.blade.php
+++ resources/views/Admin/Cursadas/edit.blade.php
@@ -1,48 +1,58 @@
 @extends('Admin.template')
 
 @section('content')
-    <div>
-
-
         <div class="edit-form-container">
-            <div class="perfil_one table">
-                <form method="post" action="{{route('admin.cursadas.update', ['cursada'=>$cursada->id])}}">
-                @csrf
-                @method('put')
+            <div class="perfil_one br">
+                <div class="perfil__header">
+                    <h2>Cursada</h2>
+                </div>
+                <div class="perfil__info">
+                    <form method="post" action="{{route('admin.cursadas.update', ['cursada'=>$cursada->id])}}">
+                    @csrf
+                    @method('put')
 
-                    <span class="perfil_dataname">Alumno: {{$cursada->alumno->nombre.' '.$cursada->alumno->apellido}}</span>
-                    <span class="perfil_dataname">Materia: {{$cursada->asignatura->nombre}}</span>
-                    <span class="perfil_dataname">Año de cursada: <input class="campo_info" value="{{$cursada->anio_cursada}}" name="anio_cursada"></span>
-                    <span class="perfil_dataname">Condicion: 
-                        <select class="campo_info" name="condicion">
+                    <div class="perfil_dataname">
+                        <label>Alumno:</label>
+                        <span class="campo_info2">{{$cursada->alumno->nombre.' '.$cursada->alumno->apellido}}</span>
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Materia:</label>
+                        <span class="campo_info2">{{$cursada->asignatura->nombre}}</span>
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Año de cursada:</label>
+                        <input class="campo_info rounded" value="{{$cursada->anio_cursada}}" name="anio_cursada">
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Condicion:</label>
+                        <select class="campo_info rounded" name="condicion">
                             <option @selected($cursada->condicion==0) value="0">Regular</option>
                             <option @selected($cursada->condicion==1) value="1">Libre</option>
                             <option @selected($cursada->condicion==2) value="2">Promocion</option>    
                             <option @selected($cursada->condicion==3) value="3">Equivalencia</option>
                             <option @selected($cursada->condicion==4) value="4">Desertor</option>
-                        </select>    
-                    </span>
-                    
-                    <span class="perfil_dataname">Aprobada:
-                        <select class="campo_info" name="aprobada">
+                        </select> 
+                    </div>
+                    <div class="perfil_dataname">
+                        <label>Aprobada:</label>
+                        <select class="campo_info rounded" name="aprobada">
                             <option @selected($cursada->aprobada==1) value="1">Si</option>
                             <option @selected($cursada->aprobada==2) value="2">No</option>
                             <option @selected($cursada->aprobada==3) value="3">Vacio/cursando</option>
-                        </select>    
-                    </span>
+                        </select>
+                    </div>
 
-                    <div class="upd"><input type="submit" value="Actualizar" class="btn_borrar"></div>
-                </form>
-
-                <form method="post" action="{{route('admin.cursadas.destroy', ['cursada'=>$cursada->id])}}">
-                    @csrf
-                    @method('delete')
-                    <div class="upd"><input type="submit" value="Borrar cursada" class="btn_borrar wit2"></div>
-                </form>
-       
+                    <div class="upd">
+                        <input type="submit" value="Actualizar" class="btn_borrar">
+                        <form method="post" action="{{route('admin.cursadas.destroy', ['cursada'=>$cursada->id])}}">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="Borrar" class="btn_borrar">
+                        </form>
+                    </div>
+                    </form>
+                </div>
             </div>
-
         </div>
-</div>
 
 @endsection
