3f2301d6ef77a5e056f607a35d9808f7062e9019
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-10-23 20:02:15 -0300

    Update index.blade.php

--- resources/views/Admin/DiasHabiles/index.blade.php
+++ resources/views/Admin/DiasHabiles/index.blade.php
@@ -1,13 +1,17 @@
 @extends('Admin.template')
 
 @section('content')
-    <div class="perfil_one table">
-        <h1>Días habiles</h1>
+    <div class="perfil_one br">
+        <div class="perfil__header">
+            <h2>Días hábiles</h2>
+        </div>
+        <div class="matricular">
             <form method="POST" action="{{route('admin.habiles.store')}}">
                 @csrf
-                    <input name="fecha" type="date">
+                    <input class="rounded" name="fecha" type="date">
                     <div class="upd"><button class="btn_edit">Agregar</button></div>
             </form>
+        
             <ul>
                 @foreach ($habiles as $habil)
                 <li>{{$habil->fecha}}</li>
@@ -19,5 +23,6 @@
                     </form>
                 @endforeach
             </ul>
+            </div>
     </div>
 @endsection
