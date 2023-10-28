047ac4ca3b21612b7a642ad3ff7bca953419955e
matias015 <78274208+matias015@users.noreply.github.com>
2023-10-27 23:59:20 -0300

    Update Correlativa.php

--- app/Models/Correlativa.php
+++ app/Models/Correlativa.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Correlativa extends Model
 {
@@ -17,5 +18,22 @@ class Correlativa extends Model
         return $this->BelongsTo(Asignatura::class,'asignatura_correlativa','id');
     }
 
+    static function debeExamenesCorrelativos($asignatura){
+        $asignatura = Asignatura::with('correlativas.asignatura')
+        ->where('id', $asignatura->id)
+        ->first();    
+     
+        $sinAprobar = [];
+
+        foreach($asignatura->correlativas as $correlativa){
+           $asigCorr = $correlativa->asignatura;
+     
+           if($asigCorr->aproboExamen(Auth::user())) continue;
+           else $sinAprobar[] = $asigCorr;
+        }
+     
+        if(count($sinAprobar)>0) return $sinAprobar;
+        else return false;
+     }
 
 }
