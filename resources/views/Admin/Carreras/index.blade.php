1ced930aa60faa0f0e2bfa9dd06f37aaf338ba45
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-11-06 10:37:44 -0300

    Update index.blade.php

--- resources/views/Admin/Carreras/index.blade.php
+++ resources/views/Admin/Carreras/index.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
 
-<div class="contenedor_top bg-transparent flex items-end gap-2">
-
-    <a href="{{route('admin.carreras.create')}}"><button class="nuevo_alumno">Agregar carrera</button></a>
-    
     <div class="contenedor-tabla_botonera">
         <form class="none grid lg-block form-hh" action="{{route('admin.carreras.index')}}">
             <div class="tabla_botonera gap-5 flex items-end">
@@ -13,7 +9,7 @@
                 <div class="contenedor_ordenar">
                     <span class="categoria">Ordenar</span>
                     <div>
-                        <select class="ordenar border-none rounded p-1 bg-white shadow" name="orden">
+                        <select class="ordenar border-none  p-1 shadow" name="orden">
                             <option @selected($filtros['orden'] == 'nombre') value="nombre">Nombre</option>
                         </select>
                     </div>
@@ -21,7 +17,7 @@
                 <div class="contenedor_filtrar">
                     <span class="categoria">Mostrar</span> 
                     <div>
-                        <select class="filtrar border-none rounded p-1 bg-white shadow" name="campo">
+                        <select class="filtrar border-none p-1 shadow" name="campo">
                             <option value="">Vigentes</option>
                             <option @selected($filtros['campo'] == 'todas') value="ninguno">Todas</option>
                         </select>
@@ -29,11 +25,11 @@
                 </div>
 
                 <div class="contenedor_filtrado">
-                    <input placeholder="Encontrar filtro..." class="filtrado-busqueda border-none rounded p-1 bg-white shadow" value="{{$filtros['filtro']}}" name="filtro" type="text">
+                    <input placeholder="Encontrar filtro..." class="filtrado-busqueda border-none p-1 bg-white shadow" value="{{$filtros['filtro']}}" name="filtro" type="text">
                 </div>
                 
                 <div class="contenedor_btn-busqueda">
-                    <input class="btn-buscador1 p-1 border-none rounded pointer" type="submit" value="Buscar">
+                    <input class="btn-buscador1 p-1 border-none pointer" type="submit" value="Buscar">
                 </div>
             </div>
         </form>
@@ -41,7 +37,7 @@
         <a class="none lg-block" href="{{route('admin.carreras.index')}}"><button class="quitar_filtro">Quitar filtros</button></a>
     </div>
     
-</div>
+
 
     {{-- @foreach ($alumnos->pagr as )
         
@@ -51,6 +47,9 @@
     </li> --}}
     
     <div class="table">
+        <div class="perfil__header-alt">
+            <a href="{{route('admin.carreras.create')}}"><button class="nuevo_alumno"><i class="ti ti-circle-plus"></i>Agregar carrera</button></a>
+        </div>
     <table class="table__body">
         <thead>
             <tr>
@@ -60,7 +59,7 @@
                 <th class="center">Fin</th>
                 <th>Observaciones</th>
                 <th>Acciones</th>
-                <th>Masivo</th>
+                <th colspan="2">Masivo</th>
             </tr>
         </thead>
         <tbody>
@@ -72,12 +71,9 @@
                 <td class="center">{{$carrera->vigente == 1? "Vigente":$carrera->anio_fin}}</td>
                 <td>{{$carrera->observaciones}}</td>
                 <td><a href="{{route('admin.carreras.edit', ['carrera' => $carrera->id])}}"><button class="btn_edit">Editar</button></a></td>
-                <td>
-                    <div class="flex-col blue-600">
-                        <a href="{{route('admin.cursadas.masivo',['asignatura'=>$carrera->primeraAsignatura()->id])}}">cursadas</a>
-                        <a href="{{route('admin.mesas.dual',['asignatura'=>$carrera->primeraAsignatura()->id])}}">mesas</a>
-                    </div>
+                <td><a href="{{route('admin.cursadas.masivo',['asignatura'=>$carrera->primeraAsignatura()->id])}}"><button class="btn_edit">Cursadas</button></a>
                 </td>
+                <td><a href="{{route('admin.mesas.dual',['asignatura'=>$carrera->primeraAsignatura()->id])}}"><button class="btn_edit">Mesas</button></a></td>
             </tr>
             @endforeach
         </tbody>
@@ -87,6 +83,4 @@
     <div class="w-1/2 mx-auto p-5 pagination">
         {{ $carreras->appends(request()->query())->links('Componentes.pagination') }}
     </div>
-
-
 @endsection
