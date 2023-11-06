5a284c2e7c5e51a8b5aae9cddec21ea87ca7af2a
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-11-06 10:46:11 -0300

    Update mensaje.blade.php

--- resources/views/Componentes/mensaje.blade.php
+++ resources/views/Componentes/mensaje.blade.php
@@ -1,34 +1,36 @@
 @if (Session::get('mensaje'))
-  <div class="z-index-top flex.col left-1 bottom-1 fixed">
+  <div class="z-index-top flex.col right-3 bottom-3 fixed">
     @if (is_array(Session::get('mensaje')))
         @foreach (Session::get('mensaje') as $msj)
-          <div class=" toast px-3 py-2 gap-2 my-1 flex just-between rounded bg-green-400 shadow-md">
+          <div class="toast px-3 py-2 gap-2 my-1 flex just-between items-center rounded shadow-md up-not_green">
             <span class="">{{$msj}}</span>
             <span class="close pointer">&times;</span>
           </div>
         @endforeach
     @else
-      <div class="toast px-3 py-2 gap-2 my-1 flex just-between rounded bg-green-400 shadow-md">
-              <p class="">{{ Session::get('mensaje') }}</p>
-              <span class="close">&times;</span>
+      <div class="toast px-3 py-2 gap-2 my-1 flex just-between items-center rounded shadow-md up-not_green">
+        <i class="ti ti-circle-check-filled"></i>
+        <p class="">{{ Session::get('mensaje') }}</p>
+        <span class="close pointer">&times;</span>
       </div>
     @endif
   </div>
 @endif
 
 @if (Session::get('error'))
-  <div class="z-index-top flex.col left-1 bottom-1 fixed">
+  <div class="z-index-top flex.col right-3 bottom-3 fixed">
     @if (is_array(Session::get('error')))
         @foreach (Session::get('error') as $msj)
-          <div class=" toast px-3 py-2 gap-2 my-1 flex just-between rounded bg-red-400 shadow-md">
+          <div class=" toast px-3 py-2 gap-2 my-1 flex just-between items-center rounded shadow-md up-not_red">
             <span class="">{{$msj}}</span>
             <span class="close pointer">&times;</span>
           </div>
         @endforeach
     @else
-      <div class="toast px-3 py-2 gap-2 my-1 flex just-between rounded bg-red-400 shadow-md">
+      <div class="toast px-3 py-2 gap-2 my-1 flex just-between items-center rounded shadow-md up-not_red">
+              <i class="ti ti-circle-x-filled"></i>
               <p class="">{!! Session::get('error') !!}</p>
-              <span class="close">&times;</span>
+              <span class="close pointer">&times;</span>
       </div>
     @endif
   </div>
@@ -36,7 +38,7 @@
 
 
 @if ($errors->any())
-  <div class="z-index-top toast px-2 py-1 gap-2 fixed flex rounded left-1 bottom-1 bg-red-400 shadow-md">
+  <div class="z-index-top toast px-2 py-1 gap-2 fixed flex rounded right-3 bottom-3 shadow-md up-not_red">
     <div class="flex-col">
     @foreach ($errors->all() as $error)
       <p class="">{{$error}}</p>
