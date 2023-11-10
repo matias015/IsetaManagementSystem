25ebba6b645825e045fccdec8d51ea385c47320f
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-11-10 07:54:01 -0300

    Update index.blade.php

--- resources/views/Admin/Carreras/index.blade.php
+++ resources/views/Admin/Carreras/index.blade.php
@@ -34,46 +34,46 @@
             </div>
         </form>
     
-        <a class="none lg-block" href="{{route('admin.carreras.index')}}"><button class="quitar_filtro">Quitar filtros</button></a>
+        <a class="none lg-block" href="{{route('admin.carreras.index')}}"><button class="btn_red"><i class="ti ti-backspace"></i>Quitar filtros</button></a>
     </div>
     
 
     
     <div class="table">
         <div class="perfil__header-alt">
-            <a href="{{route('admin.carreras.create')}}"><button class="nuevo_alumno"><i class="ti ti-circle-plus"></i>Agregar carrera</button></a>
+            <a href="{{route('admin.carreras.create')}}"><button class="btn_blue"><i class="ti ti-circle-plus"></i>Agregar carrera</button></a>
         </div>
     <table class="table__body">
         <thead>
             <tr>
                 <th>Carrera</td>
-                <th class="center">Resolución</th>
+                {{--<th class="center">Resolución</th>--}}
                 <th class="center">Apertura</th>
-                <th class="center">Fin</th>
-                <th>Acciones</th>
-                <th colspan="">Masivo</th>
-                <th></th>
+                <th class="center">Estado</th>
+                <th class="center">Acción</th>
+                <th class="center">Cargar</th>
+                <th class="center">Exportar</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($carreras as $carrera)
             <tr>
                 <td>{{$carrera->nombre}}</td>
-                <td class="center">{{$carrera->resolucion}}</td>
+                {{--<td class="center">{{$carrera->resolucion}}</td>--}}
                 <td class="center">{{$carrera->anio_apertura}}</td>
                 <td class="center">{{$carrera->vigente == 1? "Vigente":$carrera->anio_fin}}</td>
-                <td><a href="{{route('admin.carreras.edit', ['carrera' => $carrera->id])}}"><button class="btn_edit">Editar</button></a></td>
-                <td class="flex-col">
+                <td><a href="{{route('admin.carreras.edit', ['carrera' => $carrera->id])}}"><button class="btn_blue"><i class="ti ti-file-info"></i>Detalles</button></a></td>
+                <td class="flex-col items-center just-center spe">
                 @if ($carrera->primeraAsignatura())
-                    <a href="{{route('admin.cursadas.masivo',['asignatura'=>$carrera->primeraAsignatura()->id])}}">
-                        <button class="rounded px-1">Cursadas</button>
+                    <a class="flex just-center" href="{{route('admin.cursadas.masivo',['asignatura'=>$carrera->primeraAsignatura()->id])}}">
+                        <button class="spe-b1"><i class="ti ti-file-plus"></i>Cursadas</button>
                     </a>
-                    <a href="{{route('admin.mesas.dual',['asignatura'=>$carrera->primeraAsignatura()->id])}}">
-                        <button class="rounded px-1">Mesas</button>
+                    <a class="flex just-center" href="{{route('admin.mesas.dual',['asignatura'=>$carrera->primeraAsignatura()->id])}}">
+                        <button class="spe-b2"><i class="ti ti-file-plus"></i>Mesas</button>
                     </a>
                 @endif
-            </td>
-            <td><a href="/admin/cursantes/carrera/{{$carrera->id}}"><button class="bg-transparent blue-600">Exportar cursadas</button></a></td>
+                </td>
+                <td><a href="/admin/cursantes/carrera/{{$carrera->id}}"><button class="btn_blue"><i class="ti ti-file-download"></i>Cursadas</button></a></td>
             </tr>
             @endforeach
         </tbody>
