3d884650461ecfbd2b34b3d4a59684f511726789
lautarosanchez17 <136658134+lautarosanchez17@users.noreply.github.com>
2023-11-10 11:19:26 -0300

    Update mensaje.blade.php

--- resources/views/Componentes/mensaje.blade.php
+++ resources/views/Componentes/mensaje.blade.php
@@ -17,6 +17,29 @@
   </div>
 @endif
 
+{{---- mensaje de alerta amarillo-----}}
+
+{{---- la clase es "up-not_yellow" y el icono <i class="ti ti-alert-circle-filled"></i>-------  }}
+
+{{--@if (Session::get('mensaje'))
+  <div class="z-index-top flex.col right-3 bottom-3 fixed">
+    @if (is_array(Session::get('mensaje')))
+        @foreach (Session::get('mensaje') as $msj)
+          <div class="toast px-3 py-2 gap-2 my-1 flex just-between items-center rounded shadow-md up-not_yellow">
+            <span class="">{{$msj}}</span>
+            <span class="close pointer">&times;</span>
+          </div>
+        @endforeach
+    @else
+      <div class="toast px-3 py-2 gap-2 my-1 flex just-between items-center rounded shadow-md up-not_yellow">
+        <i class="ti ti-alert-circle-filled"></i>
+        <p class="">{{ Session::get('mensaje') }}</p>
+        <span class="close pointer">&times;</span>
+      </div>
+    @endif
+  </div>
+@endif --}}
+
 @if (Session::get('error'))
   <div class="z-index-top flex.col right-3 bottom-3 fixed">
     @if (is_array(Session::get('error')))
